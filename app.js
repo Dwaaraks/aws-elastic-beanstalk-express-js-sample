fb4df425ae066e2dbd62c8f7de3bbd5e1e0f2e38
Dwaaraks <dwaarakesh2002@gmail.com>
2023-10-29 12:41:47 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 8080;
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile('/home/dwaarakesh/aws-elastic-beanstalk-express-js-sample/index.html'));
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
