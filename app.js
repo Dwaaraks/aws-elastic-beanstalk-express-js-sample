e859ca19095ea3413e8799b856a4c70e70339fd3
Dwaaraks <dwaarakesh2002@gmail.com>
2023-10-29 12:21:18 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is the project done by R Dwaarakesh, Heeraa Ananthan, Sagana SS'));
+app.get('/', (req, res) => res.send('This is the project done by R Dwaarakesh, Heeraa Ananthan, Sagana SS. Project for devops'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
