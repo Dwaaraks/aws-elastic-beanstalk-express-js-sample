be92226c48abfce8fae11226c7602a525c3d92d7
Dwaaraks <dwaarakesh2002@gmail.com>
2023-11-04 18:23:27 +0530

    T10

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
+// Serve static files from the main directory
+app.use(express.static(__dirname));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html');
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
