2d6001bac18fc96a2feb8f346c71ed494e5cc81f
Dwaaraks <dwaarakesh2002@gmail.com>
2023-11-04 18:36:51 +0530

    t11

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 8080;
 // Serve static files from the main directory
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
